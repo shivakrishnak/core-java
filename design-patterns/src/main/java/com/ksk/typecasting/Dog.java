64333f3c2b17c01ac87b58b725e360c71570f885
Shivakrishna_Kurremu <Shivakrishna_Kurremu@EPINHYDW023D.princeton.epam.com>
2020-04-17 18:18:32 +0530

    type casting

---
+++ design-patterns/src/main/java/com/ksk/typecasting/Dog.java
@@ -0,0 +1,16 @@
+package com.ksk.typecasting;
+
+public class Dog extends Animal {
+	
+	public Dog() {
+		System.out.println("dog created...");
+	}
+	
+	public void walk() {
+		System.out.println("dog walking...");
+	}
+	
+	public void bark() {
+		System.out.println("dog barking...");
+	}
+}
