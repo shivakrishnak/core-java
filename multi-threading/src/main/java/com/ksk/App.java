5dd262a9cfdb697566de2c9dc2482698406d051a
Shiva Krishna <itzshivakrishna@gmail.com>
2021-02-18 16:12:00 +0530

    updates

--- multi-threading/src/main/java/com/ksk/App.java
+++ multi-threading/src/main/java/com/ksk/App.java
@@ -6,8 +6,29 @@ package com.ksk;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
         System.out.println( "Hello World!" );
+        A a = new A();
+        a.wait();
+        System.out.println( "wait" );
+        a.notify();
+        System.out.println( "notify" );
     }
+}
+
+class A implements Runnable{
+
+	@Override
+	public void run() {
+		try {
+			wait();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("thread running");
+		
+	}
+	
 }
